34543e5002e79d30cef5a64850870de86e280626
gasparteixeira <gaspar.teixeira@gmail.com>
2016-04-30 11:43:58 -0300

    criado o projeto para ajustar hello world

--- src/main/java/edm/rs/senac/com/br/projeto_ranoi/MainActivity.java
+++ src/main/java/edm/rs/senac/com/br/projeto_ranoi/MainActivity.java
@@ -2,6 +2,9 @@ package edm.rs.senac.com.br.projeto_ranoi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alteraNome(View view){
+        EditText editText = (EditText)findViewById(R.id.text_nome);
+        TextView textView = (TextView) findViewById(R.id.text_world);
+        textView.setText("Hello " + editText.getText());
     }
 }
